eb4fd817a4ee04617d89ebc9e8a627eaac065f8d
Julien Metrot <jul.metrot@gmail.com>
2023-01-23 10:54:58 -0600

    Created my first Java class

---
+++ ClassDemo/src/MonJan23.java
@@ -0,0 +1,9 @@
+public class MonJan23 {
+    public static void main(String[] args) {
+        System.out.println("Hello World," + " " + "My name is Julien Metrot");
+    }
+}
+
+class WedJan25 {
+
+}
