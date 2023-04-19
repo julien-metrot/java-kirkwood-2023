77a098e038e4c7cbb8abd28a9204188e8e1bfc54
Julien Metrot <jul.metrot@gmail.com>
2023-04-18 21:09:06 -0500

    CodeGym Level 8

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0705/Solution.java
@@ -0,0 +1,16 @@
+package en.codegym.task.pro.task07.task0705;
+
+/* 
+To infinity and beyond!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0.0,3);
+        div(-0.0,3);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package en.codegym.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+/* 
+First objects
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String();
+        String str2 = new String();
+        String str3 = new String();
+        String str4 = new String();
+        String str5 = new String();
+        String str6 = new String();
+        String str7 = new String();
+        String str8 = new String();
+        String str9 = new String();
+        String str10 = new String();
+        int [] int1 = new int[0];
+        int [] int2 = new int[0];
+        int [] int3 = new int[0];
+        int [] int4 = new int[0];
+        int [] int5 = new int[0];
+        Scanner scanner1 = new Scanner("");
+        Scanner scanner2 = new Scanner("");
+    }
+}
